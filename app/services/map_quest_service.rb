399cc17d756e05c5d206b4fea9f269156542c3c3
Caden Jarrett <caden.jarrett99@gmail.com>
2022-08-06 16:10:36 -0600

    Created map quest service with no poros

---
+++ app/services/map_quest_service.rb
@@ -0,0 +1,14 @@
+class MapQuestService
+
+    def self.conn 
+        Faraday.new(url: 'http://www.mapquestapi.com') do |faraday|
+            faraday.params['key'] = ENV['map-api-key']
+        end
+    end
+
+    def self.find_geocode(location)
+        response = conn.get("/geocoding/v1/address?location=#{location}")
+        data = JSON.parse(response.body, symbolize_names: true)
+        data[:results][0][:locations][0][:latLng]
+    end
+end
