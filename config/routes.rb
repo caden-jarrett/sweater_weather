d7cdc852fbb0539e18fbdd097f50958f358b0650
Caden Jarrett <caden.jarrett99@gmail.com>
2022-08-08 09:22:13 -0600

    Added route for final

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :api do 
     namespace :v1 do
       get '/forecast', to: 'forecast#index'
+
+      get '/munchies', to: 'munchies#index'
     end
   end
 end
