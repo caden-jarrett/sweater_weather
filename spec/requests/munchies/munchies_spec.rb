418f28a776830cc84c340085fc70c2b6387425a3
Caden Jarrett <caden.jarrett99@gmail.com>
2022-08-08 09:21:15 -0600

    Wrote request spec for final assesment

---
+++ spec/requests/munchies/munchies_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe 'Munchies API' do
+    describe 'happy path' do
+        # before :each do 
+        #     response = File.read('/mocks/forecast.json')
+        #     @data = JSON.parse(response.body, symbolize_names: true)
+        # end
+
+        it 'send a current, daily, and hourly weather report' do
+
+            get '/api/v1/munchies?location=denver,co&food=chinese'
+
+            expect(response.status).to eq(200)
+
+            destination = JSON.parse(response.body, symbolize_names: true)
+
+            expect(destination[:id]).to eq(nil)
+            expect(destination[:type]).to eq("munchie")
+            expect(destination[:attributes]).to be_a(Hash)
+            expect(destination[:attributes][:destination_city]).to be('denver,co')
+            expect(destination[:attributes][:forecast]).to be_a(Hash)
+            expect(destination[:attributes][:forecast][:summary]).to be_a(String)
+            expect(destination[:attributes][:forecast][:temperature]).to be_a(String)
+            expect(destination[:attributes][:restaurant]).to be_a(Hash)
+            expect(destination[:attributes][:restaurant][:name]).to be_a(String)
+            expect(destination[:attributes][:restaurant][:address]).to be_a(String)
+        end
+    end
+end
