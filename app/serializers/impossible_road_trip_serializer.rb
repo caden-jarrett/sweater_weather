221828442472e4df1d2f464e0e5640b95a6c56a8
Caden Jarrett <caden.jarrett99@gmail.com>
2022-08-09 17:02:45 -0600

    Added serializer for road trip format

---
+++ app/serializers/impossible_road_trip_serializer.rb
@@ -0,0 +1,20 @@
+class ImpossibleRoadTripSerializer 
+    def self.format(travel, weather_at_eta)
+
+        {
+        data: {
+                id: nil,
+                type: 'roadtrip',
+                attributes: {
+                    start_city: travel.origin,
+                    end_city: travel.destination,
+                    travel_time: travel.duration,
+                    weather_at_eta: {
+                        temperature: weather_at_eta,
+                        conditions: weather_at_eta
+                    }
+                }
+            }
+        }
+    end
+end
