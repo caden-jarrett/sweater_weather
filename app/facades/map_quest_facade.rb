2caa12823c4a61cf8adadfed121d5d5c4726f52f
Caden Jarrett <caden.jarrett99@gmail.com>
2022-08-09 16:58:32 -0600

    added map quest facade method to format differently

--- app/facades/map_quest_facade.rb
+++ app/facades/map_quest_facade.rb
@@ -2,4 +2,8 @@ class MapQuestFacade
     def self.get_geocode(location)
         MapQuestService.find_geocode(location)
     end
+
+    def self.get_travel_info(origin, destination)
+        MapQuestService.travel_info(origin, destination)
+    end
 end
