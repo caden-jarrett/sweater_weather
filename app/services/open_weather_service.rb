6fd2ca9ad310ba2f45d3fe23de75974db707d2b2
Caden Jarrett <caden.jarrett99@gmail.com>
2022-08-09 16:59:58 -0600

    added open weather  method for formatting

--- app/services/open_weather_service.rb
+++ app/services/open_weather_service.rb
@@ -14,4 +14,14 @@ class OpenWeatherService
 
         Forecast.new(data)
     end
+
+    def self.get_travel_weather(geocode)
+        response = conn.get("/data/2.5/onecall") do |f|
+            f.params['lat'] = geocode[:lat]
+            f.params['lon'] = geocode[:lng]
+            f.params['units'] = 'imperial'
+            f.params['appid'] = ENV['forecast_key']
+        end
+        data = JSON.parse(response.body, symbolize_names: true)
+    end
 end
